382cae1d0dfa594c8d654312c34dfea83d37459f
Bitok85 <kan041185@gmail.com>
2022-04-05 12:56:18 +0300

    2. Парсинг HTML страницы. [#260358 #284139]

--- src/main/java/grabber/HabrCareerParse.java
+++ src/main/java/grabber/HabrCareerParse.java
@@ -14,7 +14,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.*;
 
-public class HabrCareerParse implements DateTimeParse {
+public class HabrCareerParse {
 
     private  static final String SOURCE_LINK = "https://career.habr.com";
     private static final String PAGE_LINK =
@@ -27,45 +27,12 @@ public class HabrCareerParse implements DateTimeParse {
         Document document = connection.get();
         Elements rows = document.select(".vacancy-card__inner");
         rows.forEach(row -> {
-            Element dateElement = row.select(".vacancy-card__date").first();
+            Element dateElement = row.select(".vacancy-card__date").first().child(0);
             Element titleElement = row.select(".vacancy-card__title").first();
             Element linkElement = titleElement.child(0);
             String vacancyName = titleElement.text();
-            LocalDate localDate = habrCareerParse.localDateParse(dateElement.text());
             String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-            System.out.println(String.join(
-                    " ",
-                    vacancyName,
-                    link,
-                    DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG).format(localDate)
-            ));
+            System.out.printf("%s %s%n %s%n", vacancyName, link, dateElement.attr("datetime"));
         });
-    }
-
-    @Override
-    public LocalDate localDateParse(String parse) {
-        if (convertMap.isEmpty()) {
-            convertMonths();
-        }
-        int year = Year.now().getValue();
-        String[] parseArr = parse.split(" ");
-        int month = convertMap.get(parseArr[1]);
-        int day = Integer.parseInt(parseArr[0]);
-        return LocalDate.of(year, month, day);
-    }
-
-    private void convertMonths() {
-        Integer count = 1;
-        List<String> months = List.of(
-                "января", "февраля",
-                "марта", "апреля", "мая",
-                "июня", "июля", "августа",
-                "сентября", "октября", "ноября",
-                "декабря"
-        );
-        for (String month : months) {
-            convertMap.put(month, count);
-            count++;
-        }
     }
 }
