0a42bcdd7e7b214c6306c5dfa3bc4b2a2e108d6a
Bitok85 <kan041185@gmail.com>
2022-04-06 18:58:06 +0300

    2.2. Модель данных - Post. [#285211 #285305]

--- src/main/java/grabber/Post.java
+++ src/main/java/grabber/Post.java
@@ -18,6 +18,13 @@ public class Post {
         this.created = created;
     }
 
+    public Post(String title, String link, String description, LocalDateTime created) {
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.created = created;
+    }
+
     public int getId() {
         return id;
     }
@@ -67,14 +74,11 @@ public class Post {
             return false;
         }
         Post post = (Post) o;
-        return id
-                == post.id && Objects.equals(title, post.title)
-                && Objects.equals(link, post.link)
-                && Objects.equals(created, post.created);
+        return id == post.id && Objects.equals(link, post.link);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, link, created);
+        return Objects.hash(id, link);
     }
 }
