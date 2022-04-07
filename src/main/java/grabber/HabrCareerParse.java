47dcec1836f907e890ba1be00a66de4450edf9cf
Bitok85 <kan041185@gmail.com>
2022-04-07 21:27:07 +0300

    2.4. SqlRuParse [#285213 #285653]

--- src/main/java/grabber/HabrCareerParse.java
+++ src/main/java/grabber/HabrCareerParse.java
@@ -6,17 +6,27 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.job4j.quartz.AlertRabbit;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.*;
 
-public class HabrCareerParse {
+public class HabrCareerParse implements Parse {
 
+    private static final Logger LOG = LoggerFactory.getLogger(HabrCareerParse.class.getName());
     private  static final String SOURCE_LINK = "https://career.habr.com";
     private static final String PAGE_LINK =
             String.format("%s/vacancies/java_developer", SOURCE_LINK);
+    private static final int PAGES_AMOUNT = 5;
+    private final HarbCareerDateTimeParser dateTimeParser;
 
+    public HabrCareerParse(HarbCareerDateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+/**
     public static void main(String[] args) throws IOException {
         HarbCareerDateTimeParser dtParser = new HarbCareerDateTimeParser();
         for (int i = 1; i <= 5; i++) {
@@ -47,24 +57,62 @@ public class HabrCareerParse {
             });
         }
     }
+ */
 
-    public Post postParse(Element element) {
-        HarbCareerDateTimeParser dtParser = new HarbCareerDateTimeParser();
-        Element titleElement = element.select(".vacancy-card__title").first();
-        String title = titleElement.text();
-        String link = String.format("%s%s", SOURCE_LINK, titleElement.child(0).attr("href"));
-        Element descriptionElement =
-                element.select(".vacancy-card__icon-link").first().child(0);
-        String description = String.format("%s%s", link, descriptionElement.attr("href"));
-        Element dateElement = element.select(".vacancy-card__date").first().child(0);
-        LocalDateTime created = dtParser.localDateParse(dateElement.attr("datetime"));
-        return new Post(title, link, description, created);
+    @Override
+    public List<Post> list(String link) {
+        List<Post> resultList = new ArrayList<>();
+        try {
+            for (int i = 1; i <= PAGES_AMOUNT; i++) {
+                Connection connection =
+                        Jsoup.connect(String.format("%s%s", PAGE_LINK, "?page=" + i));
+                Document document = connection.get();
+                Elements rows = document.select(".vacancy-card__inner");
+                rows.forEach(row -> {
+                    try {
+                        resultList.add(postParse(row));
+                    } catch (IOException e) {
+                        LOG.error("I0Exception", e);
+                    }
+                });
+            }
+        } catch (IOException e) {
+            LOG.error("I0Exception", e);
+        }
+        return resultList;
     }
 
-    private static String retrieveDescription(String link) throws IOException {
+    public Post postParse(Element element) throws IOException {
+        return new Post(
+                retrieveTitle(element),
+                retrieveLink(element),
+                retrieveDescription(element),
+                retrieveDateTime(element)
+        );
+    }
+
+    private String retrieveTitle(Element element) {
+        return element.select(".vacancy-card__title").first().text();
+    }
+
+    private String retrieveLink(Element element) {
+        return element.select(".vacancy-card__title").first().child(0).attr("href");
+    }
+
+    private String retrieveDescription(Element element) throws IOException {
+        Element descriptionElement =
+                element.select(".vacancy-card__icon-link").first().child(0);
+        String link = String.format("%s%s", SOURCE_LINK, descriptionElement.attr("href"));
         Connection connection = Jsoup.connect(link);
         Document document = connection.get();
         return document.select(".job_show_description__body").first().text();
+    }
+
+    private LocalDateTime retrieveDateTime(Element element) {
+        HarbCareerDateTimeParser dtParser = new HarbCareerDateTimeParser();
+        Element dateElement =
+                element.select(".vacancy-card__date").first().child(0);
+        return dtParser.localDateParse(dateElement.attr("datetime"));
     }
 }
 
