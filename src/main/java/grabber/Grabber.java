d4875899a55769ddc6ec1cf0c3817092b82ca1ed
Bitok85 <kan041185@gmail.com>
2022-04-09 17:59:07 +0300

    6. Grabber. [#289477]

---
+++ src/main/java/grabber/Grabber.java
@@ -0,0 +1,91 @@
+package grabber;
+
+import grabber.utils.HarbCareerDateTimeParser;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.List;
+import java.util.Properties;
+
+import static org.quartz.JobBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+public class Grabber implements Grab {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Grabber.class.getName());
+    private final Properties config = new Properties();
+    private Connection cn;
+
+    public Store store() {
+        return new PsqlStore(cn);
+    }
+
+    public Scheduler scheduler() throws SchedulerException {
+        Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+        scheduler.start();
+        return scheduler;
+    }
+
+    public void cfg() {
+        try (InputStream in = Grabber.class
+                .getClassLoader()
+                .getResourceAsStream("post.properties")) {
+            config.load(in);
+            Class.forName(config.getProperty("driver-class-name"));
+            cn = DriverManager.getConnection(
+                    config.getProperty("url"),
+                    config.getProperty("username"),
+                    config.getProperty("password")
+            );
+        } catch (Exception e) {
+            LOG.error("Connection initialization", e);
+        }
+    }
+
+    @Override
+    public void init(Parse parse, Store store, Scheduler scheduler) throws SchedulerException {
+        JobDataMap data = new JobDataMap();
+        data.put("store", store);
+        data.put("parse", parse);
+        data.put("parseUrl", config.getProperty("parseUrl"));
+        JobDetail job = newJob(GrabJob.class)
+                .usingJobData(data)
+                .build();
+        SimpleScheduleBuilder times = simpleSchedule()
+                .withIntervalInSeconds(Integer.parseInt(config.getProperty("time")))
+                .repeatForever();
+        Trigger trigger = newTrigger()
+                .startNow()
+                .withSchedule(times)
+                .build();
+        scheduler.scheduleJob(job, trigger);
+
+    }
+
+    public static class GrabJob implements Job {
+
+        @Override
+        public void execute(JobExecutionContext context) throws JobExecutionException {
+            JobDataMap map = context.getJobDetail().getJobDataMap();
+            Store store = (Store) map.get("store");
+            Parse parse = (Parse) map.get("parse");
+            String parseUrl = (String) map.get("parseUrl");
+            List<Post> postList = parse.list(parseUrl);
+            postList.forEach(store::save);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Grabber grab = new Grabber();
+        grab.cfg();
+        Scheduler scheduler = grab.scheduler();
+        Store store = grab.store();
+        grab.init(new HabrCareerParse(new HarbCareerDateTimeParser()), store, scheduler);
+    }
+}
